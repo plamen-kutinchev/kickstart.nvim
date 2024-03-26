a975594c2c8c6aad2685b7d5d4f02543f7f50baa
Plamen Kutinchev <plamen.kutinchev@sumup.com>
2024-03-26 09:07:23 +0200

    configure neo-tree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,15 +1,24 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
         },
-        config = function()
-            vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
-            vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-        end,
-	}
+      }
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
+      vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
+    end,
+  },
 }
