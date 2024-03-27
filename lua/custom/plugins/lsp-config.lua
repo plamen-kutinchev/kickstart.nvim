ad840873be041362d3423a4e7a2288c1fa913904
Plamen Kutinchev <plamen.kutinchev@sumup.com>
2024-03-27 09:46:25 +0200

    latest working setup

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -1,62 +1,65 @@
-local util = require "lspconfig/util"
+local util = require 'lspconfig/util'
 
 return {
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     lazy = false,
     config = function()
-      require("mason").setup()
+      require('mason').setup()
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    'williamboman/mason-lspconfig.nvim',
     lazy = false,
     opts = {
       auto_install = true,
     },
   },
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+      local lspconfig = require 'lspconfig'
+      lspconfig.tsserver.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.solargraph.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.html.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.pyright.setup {
+        capabilities = capabilities,
+      }
 
-    lspconfig.gopls.setup({
-      capabilities = capabilities,
-      settings = {
-        gopls = {
-          analyses = {
-            nilness = true,
-            unusedparams = true,
-            unusedvariable = true,
-            unusedwrite = true,
-            useany = true,
+      lspconfig.gopls.setup {
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            analyses = {
+              nilness = true,
+              unusedparams = true,
+              unusedvariable = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            gofumpt = true,
+            staticcheck = true,
+            usePlaceholders = true,
           },
-          gofumpt = true,
-          staticcheck = true,
-          usePlaceholders = true,
         },
-      },
-    })
+      }
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end,
   },
 }
