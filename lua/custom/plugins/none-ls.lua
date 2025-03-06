37516a9e8f732c3d12570f050dd03878124b3dac
Plamen Kutinchev <plamen.kutinchev@sumup.com>
2025-03-06 08:53:10 +0200

    remove js linting breaking files

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -6,9 +6,9 @@ return {
       sources = {
         null_ls.builtins.diagnostics.markdownlint,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier.with {
-          filetypes = { 'html', 'json', 'markdown' },
-        },
+        -- null_ls.builtins.formatting.prettier.with {
+        --  filetypes = { 'html', 'json', 'markdown' },
+        -- },
         null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
